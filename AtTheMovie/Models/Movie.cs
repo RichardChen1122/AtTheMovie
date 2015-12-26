7d575eb60a44b8b6f095880f9f2aac81424a1a06
RichardChen <rchen89@126.com>
2015-12-27 00:48:07 +0800

    update

---
+++ AtTheMovie/Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AtTheMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+    }
+    public class MovieDbContext:DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
